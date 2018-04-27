1adc63d41522a015c8162303c48856414313878c
peng <997450807@qq.com>
2018-04-27 22:39:54 +0800

    棋盘布局

---
+++ board/board.js
@@ -0,0 +1,17 @@
+/**
+ * Created by PWJ on 2018/4/27.
+ */
+
+(function () {
+
+    function Board() {
+
+    }
+
+    Board.prototype.paint = function () {
+        console.log("123");
+    };
+
+    window.Board = Board;
+
+})(Window);
---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by PWJ on 2018/4/26.
+ */
+
+function Gobang() {
+    
+}
+
+Gobang.prototype.start = ()=>{
+
+};
+
+Gobang.prototype.render = ()=>{
+
+};
