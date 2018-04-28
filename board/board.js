51b2ac585bc8d8e4c9d46917e1eb27bb9b80c16c
幻影 <997450807@qq.com>
2018-04-28 18:53:54 +0800

    更新

--- board/board.js
+++ board/board.js
@@ -1,17 +1,35 @@
 /**
  * Created by PWJ on 2018/4/27.
+
+	棋子类
  */
 
 (function () {
 
-    function Board() {
+
+
+    function Board(color,size,x,y) {
+    	console.log(window);
+
+    	this.color = color;
+    	this.left = parseInt(x) - parseInt(size)/2;
+    	this.top = parseInt(y) - parseInt(size)/2;
 
     }
 
     Board.prototype.paint = function () {
         console.log("123");
+
+        let div = document.createElement("div");
+
+        div.className = "board";
+        div.style.left = this.left +"px";
+        div.style.top = this.top +"px";
+
+        return div;
+
     };
 
     window.Board = Board;
 
-})(Window);
+})(window);
--- index.js
+++ index.js
@@ -2,14 +2,44 @@
  * Created by PWJ on 2018/4/26.
  */
 
-function Gobang() {
-    
-}
+(function(){
 
-Gobang.prototype.start = ()=>{
+	//棋子大小
+	let boardSize = 40;
+	//棋盘网格 15*15
+	let boardGridx = 15;
+	let boardGridy = 15;
 
-};
+	function Gobang(dom) {
+    	this.dom = dom;
 
-Gobang.prototype.render = ()=>{
+    	console.log(this);
+	}
 
-};
+	Gobang.prototype.start = function(){
+
+		this.render();
+	}
+
+	Gobang.prototype.render = function(){
+		for(let m=0;m<boardGridx;m++){
+
+			for(let n =0;n<boardGridy;n++){
+				let board = new Board("transpant",40,m*40,n*40);
+				this.addBoard(board);
+			}
+
+		}
+	};
+
+	Gobang.prototype.addBoard = function(board){
+		console.log(board.paint());
+		console.log(this.dom);
+		this.dom.appendChild(board.paint());
+	}
+
+	window.Gobang = Gobang;
+})(window)
+
+	
+
