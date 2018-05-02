1cfd9df6455560236197ca683f25374577710173
幻影 <997450807@qq.com>
2018-05-02 18:57:43 +0800

    提交

--- board/board.js
+++ board/board.js
@@ -6,29 +6,36 @@
 
 (function () {
 
-
-
     function Board(color,size,x,y) {
-    	console.log(window);
-
     	this.color = color;
     	this.left = parseInt(x) - parseInt(size)/2;
     	this.top = parseInt(y) - parseInt(size)/2;
-
+        this.dom = null;
     }
 
     Board.prototype.paint = function () {
-        console.log("123");
 
         let div = document.createElement("div");
 
         div.className = "board";
         div.style.left = this.left +"px";
         div.style.top = this.top +"px";
+        div.style.backgroundColor = this.color;
 
+        this.dom = div;
         return div;
-
     };
+
+
+    Board.prototype.remove = function(){
+
+        console.log(this);
+
+        this.color = "transparent";
+        this.dom.style.backgroundColor = "transparent";
+
+        console.log(this.dom);
+    }
 
     window.Board = Board;
 
--- index.js
+++ index.js
@@ -2,6 +2,27 @@
  * Created by PWJ on 2018/4/26.
  */
 
+
+function judgeFuc(args,boardSize){
+	let  newArgs = args.sort();
+	let i = 0;
+
+	for(let m=0;m<newArgs.length-1;m++){
+		if(i < 5){
+		  if(newArgs[m - 1]-newArgs[m] === boardSize){
+		  		i++;
+		  }else{
+		  	i = 0;
+		  }
+		}
+		return false;
+	}
+
+	return true;
+};
+
+
+
 (function(){
 
 	//棋子大小
@@ -9,23 +30,23 @@
 	//棋盘网格 15*15
 	let boardGridx = 15;
 	let boardGridy = 15;
+	//确定黑白子
+     let isBlack = true;
+
 
 	function Gobang(dom) {
     	this.dom = dom;
-
-    	console.log(this);
+    	this.board = [];
 	}
 
 	Gobang.prototype.start = function(){
-
 		this.render();
 	}
 
 	Gobang.prototype.render = function(){
 		for(let m=0;m<boardGridx;m++){
-
 			for(let n =0;n<boardGridy;n++){
-				let board = new Board("transpant",40,m*40,n*40);
+				let board = new Board("transparent",boardSize,m*boardSize,n*boardSize);
 				this.addBoard(board);
 			}
 
@@ -33,10 +54,97 @@
 	};
 
 	Gobang.prototype.addBoard = function(board){
-		console.log(board.paint());
-		console.log(this.dom);
-		this.dom.appendChild(board.paint());
+		let div = board.paint();
+		this.dom.appendChild(div);
+
+		div.onclick = ()=>{
+            if(board.color !== "transparent"){return false;}
+
+            if(isBlack){
+                div.style.backgroundColor = "black";
+                board.color = "black";
+            }else{
+                div.style.backgroundColor = "white";
+                board.color = "white";
+            }
+            isBlack = !isBlack;
+            this.whoWin(board);
+		};
 	}
+
+
+	Gobang.prototype.whoWin = function(board){
+		
+		let winArgs = [];
+		let args = this.board.filter(index=>{
+			return index.color === board.color ;
+		});
+
+		this.board.push(board);
+		if(args.length < 4){return false;}
+
+		let x = board.left;
+		let y = board.top;
+
+		let argsX = [] // X轴 Y相同;
+		let argsY = [] // Y轴 X相同;
+        let argsE = [] // 一三象限   X Y相等 
+        let argsO = [] // 二四象限   X Y相导 
+
+		for(let index of args){
+			let x1 =  index.left;
+			let y1 =  index.top;
+			if( x === x1){
+				argsX.push(y1);
+			}else if(y === y1){
+				argsY.push(x1);
+			}else if( x - x1 === y - y1 ){
+				argsE.push(x1);
+			}else if(x-x1 === -(y-y1)){
+				argsO.push(x1);
+			}
+		}
+
+		if(argsX.length < 4 && argsY.length < 4 && argsE.length < 4 && argsO.length < 4 ){
+			return false;
+		}else{
+			if(argsX.length >= 4){
+				argsX.push(y);
+			}
+			if(argsE.length >= 4){
+				argsE.push(x);
+			}
+			if(argsY.length >= 4){
+				argsY.push(x);
+			}
+			if(argsO.length >= 4){
+				argsO.push(x);
+			}
+		}
+
+		if(judgeFuc(argsX,boardSize) || judgeFuc(argsY,boardSize) || judgeFuc(argsE,boardSize) || judgeFuc(argsO,boardSize)){
+			this.winEvent();
+		}
+       
+	};
+
+
+	Gobang.prototype.winEvent = function(){	
+		if(isBlack){
+			alert("白方胜,游戏重新开始");
+		}else{
+			alert("黑方胜,游戏重新开始");
+		}
+
+		for(let index of this.board){
+			console.log(index);
+			index.remove();
+		}
+
+		this.board = [];
+		isBlack = true;
+	};
+
 
 	window.Gobang = Gobang;
 })(window)
